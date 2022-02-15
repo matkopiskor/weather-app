a3199f048a10fe156a19110401fa0a5d1fc67b80
Matko Piskor <matko.piskor@gmail.com>
2022-02-15 19:03:13 +0100

    App tsx setup

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
 import { render } from 'react-dom';
+
+import './styles/reset.css';
+import './styles/style.css';
 
 render(<div>Hello world</div>, document.getElementById('root'));
